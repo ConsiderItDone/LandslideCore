87c9815923d7b5e19dfc52f177ee9ed0a768c44a
n0cte <ilnur.a.g@gmail.com>
2023-03-28 18:04:36 +0400

    add databse iterator implementation

--- vm/database.go
+++ vm/database.go
@@ -9,9 +9,17 @@ var (
 	_ dbm.DB = &Database{}
 )
 
-type Database struct {
-	database.Database
-}
+type (
+	Database struct {
+		database.Database
+	}
+	Iterator struct {
+		database.Iterator
+
+		start []byte
+		end   []byte
+	}
+)
 
 func (db Database) Get(key []byte) ([]byte, error) {
 	res, err := db.Database.Get(key)
@@ -37,13 +45,11 @@ func (db Database) DeleteSync(key []byte) error {
 }
 
 func (db Database) Iterator(start, end []byte) (dbm.Iterator, error) {
-	//TODO implement me
-	panic("implement me")
+	return Iterator{db.Database.NewIteratorWithStart(start), start, end}, nil
 }
 
 func (db Database) ReverseIterator(start, end []byte) (dbm.Iterator, error) {
-	//TODO implement me
-	panic("implement me")
+	return Iterator{db.Database.NewIteratorWithStart(start), start, end}, nil
 }
 
 func (db Database) NewBatch() dbm.Batch {
@@ -59,4 +65,33 @@ func (db Database) Print() error {
 func (db Database) Stats() map[string]string {
 	//TODO implement me
 	panic("implement me")
+}
+
+func (iter Iterator) Domain() (start []byte, end []byte) {
+	return iter.start, iter.end
+}
+
+func (iter Iterator) Valid() bool {
+	return iter.Iterator.Error() == nil && len(iter.Iterator.Key()) > 0
+}
+
+func (iter Iterator) Next() {
+	iter.Iterator.Next()
+}
+
+func (iter Iterator) Key() (key []byte) {
+	return iter.Iterator.Key()
+}
+
+func (iter Iterator) Value() (value []byte) {
+	return iter.Iterator.Value()
+}
+
+func (iter Iterator) Error() error {
+	return iter.Iterator.Error()
+}
+
+func (iter Iterator) Close() error {
+	iter.Iterator.Release()
+	return iter.Error()
 }
